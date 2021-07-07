3a6cb061552704925ae307e12d8d25251ef845ab
xasopheno <danny@xasopheno.com>
2021-07-07 19:35:26 -0400

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,8 @@ import Data.Ratio (denominator, numerator)
 
 data PointOp = PointOp
   { fm :: Rational,
-    fa :: Rational,
     lm :: Rational
+    -- fa :: Rational,
     -- pm :: Rational,
     -- pa :: Rational,
     -- gm :: Rational
@@ -14,34 +14,41 @@ data PointOp = PointOp
 fmPointOp :: PointOp -> Rational -> PointOp
 fmPointOp inputOp m =
   inputOp
-    { fm = fm inputOp * m
+    { 
+      fm = fm inputOp * m
     }
 
+fmSeqNormalForm :: SeqOp -> Rational -> SeqOp
+fmSeqNormalForm inputSeq m = map helper inputSeq
+  where helper = \op -> fmPointOp op m
+
+
 instance Show PointOp where
-  show op = "Fm " ++ showRational (fm op) ++ " | Fa " ++ showRational (fa op) ++ " | Lm " ++ showRational (lm op)
+  show op = "Fm " ++ showRational (fm op) ++ " | Lm " ++ showRational (lm op)
     where
       showRational r = show (numerator r) ++ "/" ++ show (denominator r)
 
-newtype NormalForm = NormalForm [SeqNormalForm]
+data NormalForm = NormalForm [SeqOp]
 
-newtype SeqNormalForm = SeqNormalForm [PointOp]
+data SeqOp = SeqOp [PointOp]
 
 instance Show NormalForm where
   show (NormalForm seqs) = "Overlay " ++ show seqs
 
-instance Show SeqNormalForm where
-  show (SeqNormalForm pointops) = "Seq " ++ show pointops
+instance Show SeqOp where
+  show (SeqOp pointops) = "Seq " ++ show pointops
 
-op = fmPointOp o 10
+op = fmPointOp o 2
   where
-    o = PointOp {fm = 1 / 2, fa = -3, lm = 1}
+    o = PointOp {
+      fm = 1,
+      -- fa = -3, 
+      lm = 1/2
+    }
 
-normalform =
-  NormalForm
-    [ SeqNormalForm
-        [ op
-        ]
-    ]
+seqnormalform =  SeqOp [ op ]
+
+result = fmSeqNormalForm seqnormalform 2
 
 main :: IO ()
-main = print normalform
+main = print result
