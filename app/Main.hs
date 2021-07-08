2c4a6df91006b7fb10fed2d508442f3be86e0ce0
xasopheno <danny@xasopheno.com>
2021-07-08 00:04:31 -0400

    beginning of normalize

--- app/Main.hs
+++ app/Main.hs
@@ -11,26 +11,50 @@ data PointOp = PointOp
     -- gm :: Rational
   }
 
+data Op = Fm Rational | Lm Rational
+
+class Normalize a where
+  normalize :: a -> Op -> a
+
+instance Normalize PointOp where
+  normalize pointOp (Fm r) = applySimpleOp pointOp (Fm r)
+  normalize pointOp (Lm r) = applySimpleOp pointOp (Lm r)
+
+op1 =
+  PointOp
+    { fm = 1,
+      lm = 1 / 2
+    }
+
+normalized = normalize op1 (Fm 3)
+
+applySimpleOp :: PointOp -> Op -> PointOp
+applySimpleOp pointOp (Fm r) = fmPointOp pointOp r
+applySimpleOp pointOp (Lm r) = lmPointOp pointOp r
+
 fmPointOp :: PointOp -> Rational -> PointOp
 fmPointOp inputOp m =
   inputOp
-    { 
-      fm = fm inputOp * m
+    { fm = fm inputOp * m
     }
 
-fmSeqNormalForm :: SeqOp -> Rational -> SeqOp
-fmSeqNormalForm inputSeq m = map helper inputSeq
-  where helper = \op -> fmPointOp op m
+lmPointOp :: PointOp -> Rational -> PointOp
+lmPointOp inputOp m =
+  inputOp
+    { lm = lm inputOp * m
+    }
 
+fmSeqNormalForm :: [PointOp] -> Rational -> [PointOp]
+fmSeqNormalForm inputSeq m = map (\op -> applySimpleOp op (Fm m)) inputSeq
 
 instance Show PointOp where
   show op = "Fm " ++ showRational (fm op) ++ " | Lm " ++ showRational (lm op)
     where
       showRational r = show (numerator r) ++ "/" ++ show (denominator r)
 
-data NormalForm = NormalForm [SeqOp]
+newtype NormalForm = NormalForm [SeqOp]
 
-data SeqOp = SeqOp [PointOp]
+newtype SeqOp = SeqOp [PointOp]
 
 instance Show NormalForm where
   show (NormalForm seqs) = "Overlay " ++ show seqs
@@ -40,15 +64,16 @@ instance Show SeqOp where
 
 op = fmPointOp o 2
   where
-    o = PointOp {
-      fm = 1,
-      -- fa = -3, 
-      lm = 1/2
-    }
+    o =
+      PointOp
+        { fm = 1,
+          -- fa = -3,
+          lm = 1 / 2
+        }
 
-seqnormalform =  SeqOp [ op ]
+seqnormalform = [op]
 
 result = fmSeqNormalForm seqnormalform 2
 
 main :: IO ()
-main = print result
+main = print normalized
