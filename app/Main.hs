087f90aa73979656472f1b6dad6a2e1a73ede5c4
xasopheno <danny@xasopheno.com>
2021-07-08 23:16:16 -0400

    simple normalize working

--- app/Main.hs
+++ app/Main.hs
@@ -6,33 +6,44 @@ import Data.Ratio (denominator, numerator)
 
 data PointOp = PointOp
   { fm :: Rational,
-    lm :: Rational
-    -- fa :: Rational,
+    fa :: Rational,
+    lm :: Rational,
+    gm :: Rational
     -- pm :: Rational,
     -- pa :: Rational,
-    -- gm :: Rational
   }
 
-data Op = Fm Rational | Lm Rational
+data Op = Fm Rational | Fa Rational | Lm Rational | Gm Rational
 
 applySimpleOp :: PointOp -> Op -> PointOp
-applySimpleOp pointOp (Fm r) = fmPointOp pointOp r
-applySimpleOp pointOp (Lm r) = lmPointOp pointOp r
+applySimpleOp op (Fm r) = fmOp op r
+applySimpleOp op (Fa r) = faOp op r
+applySimpleOp op (Lm r) = lmOp op r
+applySimpleOp op (Gm r) = gmOp op r
 
-fmPointOp :: PointOp -> Rational -> PointOp
-fmPointOp inputOp m =
+fmOp :: PointOp -> Rational -> PointOp
+fmOp inputOp m =
   inputOp
     { fm = fm inputOp * m
     }
 
-lmPointOp :: PointOp -> Rational -> PointOp
-lmPointOp inputOp m =
-  inputOp
-    { lm = lm inputOp * m
+faOp :: PointOp -> Rational -> PointOp
+faOp op m =
+  op
+    { fa = fa op + m
     }
 
-fmSeqNormalForm :: [PointOp] -> Rational -> [PointOp]
-fmSeqNormalForm inputSeq m = map (\op -> applySimpleOp op (Fm m)) inputSeq
+lmOp :: PointOp -> Rational -> PointOp
+lmOp op m =
+  op
+    { lm = lm op * m
+    }
+
+gmOp :: PointOp -> Rational -> PointOp
+gmOp op m =
+  op
+    { gm = gm op * m
+    }
 
 instance Show PointOp where
   show op = "Fm " ++ showRational (fm op) ++ " | Lm " ++ showRational (lm op)
@@ -49,40 +60,40 @@ instance Show NormalForm where
 instance Show SeqOp where
   show (SeqOp pointops) = "Seq " ++ show pointops
 
-op = fmPointOp o 2
-  where
-    o =
-      PointOp
-        { fm = 1,
-          -- fa = -3,
-          lm = 1 / 2
-        }
-
-seqnormalform = [op]
-
-result = fmSeqNormalForm seqnormalform 2
-
 class Normalize a where
+  new :: a
   normalize :: a -> Op -> a
 
 instance Normalize PointOp where
+  new = PointOp {fm = 1, fa = 0, lm = 1, gm = 1}
   normalize pointOp (Fm r) = applySimpleOp pointOp (Fm r)
+  normalize pointOp (Fa r) = applySimpleOp pointOp (Fa r)
   normalize pointOp (Lm r) = applySimpleOp pointOp (Lm r)
+  normalize pointOp (Gm r) = applySimpleOp pointOp (Gm r)
 
-instance Normalize [PointOp] where
-  normalize seqOp op = map helper seqOp
+instance Normalize SeqOp where
+  new = SeqOp [new :: PointOp]
+  normalize (SeqOp ops) op = SeqOp result
     where
-      helper = (`normalize` op)
+      result = map (`normalize` op) ops
 
-op1 =
+instance Normalize NormalForm where
+  new = NormalForm [new :: SeqOp]
+  normalize (NormalForm seqOp) op = NormalForm result
+    where
+      result = map (`normalize` op) seqOp
+
+op =
   PointOp
     { fm = 1,
-      lm = 1 / 2
+      fa = 1,
+      lm = 1,
+      gm = 1
     }
 
-seqOp = [op1]
+normalform = new :: NormalForm
 
-normalized = normalize seqOp (Fm 3)
+normalized = normalize normalform (Fm 2)
 
 main :: IO ()
 main = print normalized
